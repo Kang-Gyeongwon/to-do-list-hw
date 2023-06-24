3e25e7d5ce463e6c06836394132a259f9369834e
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-24 15:43:45 +0900

    feat: App.js 파일에 ToDoList import 하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
-import ToDoInput from "./components/ToDoInput"; 
+import ToDoInput from "./components/ToDoInput";
+import ToDoList from "./components/ToDoList";
+
 const App = () => {
-  return <div className="App">
-    <ToDoInput />
-  </div>;
+  return (
+    <div className="App">
+      <ToDoInput />
+      <ToDoList />
+    </div>
+  );
 };
 
 export default App;

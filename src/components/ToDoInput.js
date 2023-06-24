397578261055e909866050d73e4b29e51d840618
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-24 14:57:51 +0900

    style: input, button 스타일 지정하기

--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -1,13 +1,58 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const ToDoInputBox = styled.div`
+  width: 90%;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border: 1.5px solid #dcdcdc;
+  padding: 0;
+  margin: 0 auto;
+`;
+
 const ToDoInput = () => {
   return (
-    <div>
-      <input
-        id="todo_input"
-        type="text"
-        placeholder="할일을 입력하세요&#128393;"
-      />
-      <input id="todo_btn" type="submit" value="추가" />
-    </div>
+    <Header>
+      <ToDoInputBox>
+        <input
+          id="todo_input"
+          type="text"
+          placeholder="할일을 입력하세요&#128393;"
+          style={{
+            width: "85%",
+            margin: "0",
+            paddingLeft: "1.2%",
+            border: "0",
+          }}
+        />
+        <input
+          id="todo_btn"
+          type="submit"
+          value="추가"
+          style={{
+            width: "15%",
+            height: "40px",
+            backgroundColor: "#E6E6FA",
+            border: "none",
+            marginLeft: "auto",
+            borderRadius: "0",
+            color: "#212529",
+            fontSize: "18px",
+          }}
+        />
+      </ToDoInputBox>
+    </Header>
   );
 };
 

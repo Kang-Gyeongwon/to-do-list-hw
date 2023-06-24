633ec4c412a430db3cfd6443069c20e5bc003970
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-24 17:14:46 +0900

    feat: todolist 보여주기

--- src/components/ToDo/index.js
+++ src/components/ToDo/index.js
@@ -19,39 +19,53 @@ const ToDo = () => {
     }
   };
   return (
-    <Header>
-      <ToDoInputBox>
-        <input
-          id="todo_input"
-          type="text"
-          placeholder="할일을 입력하세요&#128393;"
-          style={{
-            width: "85%",
-            margin: "0",
-            paddingLeft: "1.2%",
-            border: "0",
-          }}
-          value={curText}
-          onChange={(e) => updateText(e.target.value)}
-        />
-        <input
-          id="todo_btn"
-          type="submit"
-          value="추가"
-          style={{
-            width: "15%",
-            height: "40px",
-            backgroundColor: "#E6E6FA",
-            border: "none",
-            marginLeft: "auto",
-            borderRadius: "0",
-            color: "#212529",
-            fontSize: "18px",
-          }}
-          onClick={handleClickSubmit}
-        />
-      </ToDoInputBox>
-    </Header>
+    <div>
+      <Header>
+        <ToDoInputBox>
+          <input
+            id="todo_input"
+            type="text"
+            placeholder="할일을 입력하세요&#128393;"
+            style={{
+              width: "85%",
+              margin: "0",
+              paddingLeft: "1.2%",
+              border: "0",
+            }}
+            value={curText}
+            onChange={(e) => updateText(e.target.value)}
+          />
+          <input
+            id="todo_btn"
+            type="submit"
+            value="추가"
+            style={{
+              width: "15%",
+              height: "40px",
+              backgroundColor: "#E6E6FA",
+              border: "none",
+              marginLeft: "auto",
+              borderRadius: "0",
+              color: "#212529",
+              fontSize: "18px",
+            }}
+            onClick={handleClickSubmit}
+          />
+        </ToDoInputBox>
+      </Header>
+      <div id="body">
+        <ul>
+          {todos.map((todo, idx) => {
+            return (
+              <li key={idx}>
+                <span>{todo.content}</span>
+                <spna>&#128465;</spna>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
   );
 };
 
